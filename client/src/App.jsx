a029e260895fb8ce8cc36e0a876b93b31207a554
Heytgala <heytgala26@gmail.com>
2025-02-07 12:51:21 -0600

    Added Sub Category functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,9 @@ import fetchUserDetails from './utils/fetchUserDetails';
 import { setUserDetails } from './store/userSlice';
 import { useDispatch } from 'react-redux'
 import { useEffect } from 'react'
-
+import { setAllCategory,setAllSubCategory,setLoadingCategory } from './store/productSlice';
+import Axios from './utils/Axios'
+import SummaryApi from './common/SummaryApi'
 
 function App() {
 
@@ -20,9 +22,45 @@ function App() {
       dispatch(setUserDetails(userData.data))
   }
 
+  const fetchCategory = async()=>{
+    try {
+        dispatch(setLoadingCategory(true))
+        const response = await Axios({
+            ...SummaryApi.getCategory
+        })
+        const { data : responseData } = response
+
+        if(responseData.success){
+           dispatch(setAllCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name)))) 
+        }
+    } catch (error) {
+        
+    }finally{
+      dispatch(setLoadingCategory(false))
+    }
+  }
+
+  const fetchSubCategory = async()=>{
+    try {
+        const response = await Axios({
+            ...SummaryApi.getSubCategory
+        })
+        const { data : responseData } = response
+
+        if(responseData.success){
+           dispatch(setAllSubCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name)))) 
+        }
+    } catch (error) {
+        
+    }finally{
+    }
+  }
+
 
   useEffect(()=>{
-    fetchUser()
+    fetchUser(),
+    fetchCategory(),
+    fetchSubCategory()
   },[])
 
   return (
