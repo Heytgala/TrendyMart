9e253d636e1ffc27458875838df92c7d0f4a26a4
Heytgala <heytgala26@gmail.com>
2025-02-16 12:47:56 -0600

    Modified UI bugs

--- client/src/pages/ProductListPage.jsx
+++ client/src/pages/ProductListPage.jsx
@@ -73,24 +73,24 @@ const ProductListPage = () => {
   }, [params, AllSubCategory])
 
   return (
-    <section className='sticky top-24 lg:top-20'>
-      <div className='container sticky top-24  mx-auto grid grid-cols-[90px,1fr]  md:grid-cols-[200px,1fr] lg:grid-cols-[280px,1fr]'>
-        {/**sub category **/}
-        <div className=' min-h-[88vh] max-h-[88vh] overflow-y-scroll  grid gap-1 shadow-md scrollbarCustom bg-white py-2'>
+    <section className='mt-2 mb-2 lg:mt-2'>
+      <div className='container mx-auto flex gap-4'>
+        
+        {/** Sub Category Section (Left) **/}
+        <div className='min-h-[88vh] max-h-[88vh] overflow-y-auto w-[90px] md:w-[200px] lg:w-[280px] shadow-md scrollbarCustom bg-white py-2'>
           {
             DisplaySubCatory.map((s, index) => {
-               const link = `/${valideURLConvert(s?.category[0]?.name)}-${s?.category[0]?._id}/${valideURLConvert(s.name)}-${s._id}`
+              const link = `/${valideURLConvert(s?.category[0]?.name)}-${s?.category[0]?._id}/${valideURLConvert(s.name)}-${s._id}`
               return (
-                <Link to={link} className={`w-full p-2 lg:flex items-center lg:w-full lg:h-16 box-border lg:gap-4 border-b 
+                <Link to={link} key={s._id} className={`w-full p-2 lg:flex items-center box-border lg:gap-4 border-b 
                   hover:bg-green-100 cursor-pointer
                   ${subCategoryId === s._id ? "bg-green-100" : ""}
-                `}
-                >
-                  <div className='w-fit max-w-28 mx-auto lg:mx-0 bg-white rounded  box-border' >
+                `}>
+                  <div className='w-fit max-w-28 mx-auto lg:mx-0 bg-white rounded box-border'>
                     <img
                       src={s.image}
                       alt='subCategory'
-                      className=' w-14 lg:h-14 lg:w-12 h-full object-scale-down'
+                      className='w-14 lg:h-14 lg:w-12 h-full object-scale-down'
                     />
                   </div>
                   <p className='-mt-6 lg:mt-0 text-xs text-center lg:text-left lg:text-base'>{s.name}</p>
@@ -100,39 +100,29 @@ const ProductListPage = () => {
           }
         </div>
 
-
-        {/**Product **/}
-        <div className='sticky top-20'>
-          <div className='bg-white shadow-md p-4 z-10'>
+        {/** Product List Section (Right) **/}
+        <div className='flex-1'>
+          <div className='bg-white shadow-md p-4'>
             <h3 className='font-semibold'>{subCategoryName}</h3>
           </div>
-          <div>
-
-           <div className='min-h-[80vh] max-h-[80vh] overflow-y-auto relative'>
-            <div className=' grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 gap-4 '>
-                {
-                  data.map((p, index) => {
-                    return (
-                      <CardProduct
-                        data={p}
-                        key={p._id + "productSubCategory" + index}
-                      />
-                    )
-                  })
-                }
-              </div>
-           </div>
-
-            {
-              loading && (
-                <Loading />
-              )
-            }
-
+          <div className='min-h-[80vh] max-h-[80vh] overflow-y-auto relative'>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 gap-4'>
+              {
+                data.map((p, index) => (
+                  <CardProduct
+                    data={p}
+                    key={p._id + "productSubCategory" + index}
+                  />
+                ))
+              }
+            </div>
+            {loading && <Loading />}
           </div>
         </div>
+
       </div>
     </section>
+
   )
 }
 
